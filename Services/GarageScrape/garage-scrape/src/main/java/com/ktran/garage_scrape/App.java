fd1c153e63f72cb437ff5b2edb47c11ffe44433f
Kevin Tran <8monkeys04@gmail.com>
2026-09-16 22:30:58 -0700

    Setting up Maven Project

---
+++ Services/GarageScrape/garage-scrape/src/main/java/com/ktran/garage_scrape/App.java
@@ -0,0 +1,13 @@
+package com.ktran.garage_scrape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
